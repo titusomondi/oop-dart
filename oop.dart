97f874c7552172b55c3d7077c967cf61e2822a5c
Titus Omondi <101582094+titusomondi@users.noreply.github.com>
2024-03-16 13:18:39 +0300

    initial commit

--- oop.dart
+++ oop.dart
@@ -1,41 +1,55 @@
-# Define an interface
-class FileParser:
-    def parse(self, file_name):
-        raise NotImplementedError("Method 'parse' must be implemented")
+import 'dart:io';
 
+// Define an interface
+abstract class FileParser {
+  List<List<String>> parse(String fileName);
+}
 
-# Implement the interface
-class CSVParser(FileParser):
-    def parse(self, file_name):
-        with open(file_name, 'r') as file:
-            data = []
-            for line in file:
-                data.append(line.strip().split(','))
-            return data
+// Implement the interface
+class CSVParser implements FileParser {
+  @override
+  List<List<String>> parse(String fileName) {
+    var data = <List<String>>[];
+    var file = File(fileName);
+    var lines = file.readAsLinesSync();
+    for (var line in lines) {
+      data.add(line.split(','));
+    }
+    return data;
+  }
+}
 
+// Define a base class with inheritance
+class Animal {
+  String name;
 
-# Define a base class with inheritance
-class Animal:
-    def __init__(self, name):
-        self.name = name
+  Animal(this.name);
 
-    def speak(self):
-        raise NotImplementedError("Method 'speak' must be implemented")
+  void speak() {
+    throw UnimplementedError("Method 'speak' must be implemented");
+  }
+}
 
+// Implement a subclass that overrides a method
+class Dog extends Animal {
+  Dog(String name) : super(name);
 
-# Implement a subclass that overrides a method
-class Dog(Animal):
-    def speak(self):
-        return f"{self.name} says Woof!"
+  @override
+  void speak() {
+    print('$name says Woof!');
+  }
+}
 
+// Create an instance of a class initialized with data from a file
+void main() {
+  var fileName = 'animals.csv';
+  var parser = CSVParser();
+  var animalData = parser.parse(fileName);
 
-# Create an instance of a class initialized with data from a file
-if __name__ == "__main__":
-    file_name = "animals.csv"
-    parser = CSVParser()
-    animal_data = parser.parse(file_name)
-
-    for data in animal_data:
-        if data[0] == 'dog':
-            animal = Dog(data[1])
-            print(animal.speak())
+  for (var data in animalData) {
+    if (data[0] == 'dog') {
+      var animal = Dog(data[1]);
+      animal.speak();
+    }
+  }
+}
